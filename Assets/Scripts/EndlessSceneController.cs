5226b712f7308100522828309b852ea4c56583ca
zwang87 <zw401@nyu.edu>
2014-01-15 18:17:46 -0500

    skybox + cloud generator
    oculus rift sdk imported, implemented skybox and cloud generator

---
+++ Assets/Scripts/EndlessSceneController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessSceneController : MonoBehaviour {
+	private float prevTime = 0;
+	private float curTime = 0;
+
+	public int speed = 600;
+
+	float distance = 0;
+	Vector3 lastPos;
+	Vector3 initPos;
+	int count = 1;
+
+	GameObject cloud01;
+	GameObject cloud02;
+
+	public GameObject prefab;
+
+	bool isCloudCreated = false;
+	// Use this for initialization
+	void Start () {
+		lastPos = this.transform.position;
+		initPos = this.transform.position;
+		cloud01 = GameObject.Find("Cloud01");
+		cloud02 = GameObject.Find("Cloud02");
+		GameObject test01 = (GameObject)Instantiate(prefab, new Vector3(0, 1, 0), Quaternion.Euler(0, 0, 0));
+		GameObject test02 = (GameObject)Instantiate(prefab, new Vector3(0, 1, 2220), Quaternion.Euler(0, 0, 0));
+		Destroy(test01, 2220/speed+1);
+		Destroy(test02, 2*2220/speed+1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance += Vector3.Distance(this.transform.position, lastPos);
+		lastPos = this.transform.position;
+		if(distance > 2220){
+			//isCloudCreated = true;
+			distance = 0;
+			count++;
+			//cloud01.transform.position = new Vector3(0, 1, 4440);
+			//this.transform.position = new Vector3(0, 1, 0);
+			//GameObject cloud03 = new GameObject("Cloud03");
+			GameObject test = (GameObject)Instantiate(prefab, new Vector3(0, 1, count*2220), Quaternion.Euler(0, 0, 0));
+			Destroy(test, 2*2220/speed+1);
+			//if(test.transform.position.z < this.transform.position.z - 1110){
+			//	Destroy(test);
+			//}
+		}
+
+		this.transform.Translate(0, 0, speed*Time.deltaTime);
+
+	}
+}
